91802fae40e7d6d43f9743ce5f34f407ea4a1dd8
Jeremiah Utecht <jeremiah@peanutbuttr.com>
2016-03-06 08:57:31 -0600

    Cleaned up GeoCode Success Responses Now

--- lib/geo_code.rb
+++ lib/geo_code.rb
@@ -1,17 +1,16 @@
 require "httparty"
-require "pp"
 
 class GeoCode
   include HTTParty
-  base_uri "maps.googleapis.com/maps/api/geocode/"
+  base_uri "https://maps.googleapis.com/maps/api/geocode/"
 
 # TODO: This works in development but needs to be done better
   def self.api_key
     Rails.application.secrets.google_geocode_key
   end
 
-  def self.postal_code(postal, options = {})
-    url = "json?address=#{postal}&key=#{api_key}"
-    GeoCode.get(url, options)
+  def self.postal_code(postal)
+    url = "/json?address=#{postal}&key=#{api_key}"
+    GeoCode.get(url)["results"]
   end
 end
