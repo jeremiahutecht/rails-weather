0c39aeb7980512d92f6a3776288837512f809383
Jeremiah Utecht <jeremiah@peanutbuttr.com>
2016-03-06 08:21:00 -0600

    API Class now returning 404's

--- lib/geo_code.rb
+++ lib/geo_code.rb
@@ -1,28 +1,17 @@
-require 'httparty'
+require "httparty"
+require "pp"
 
 class GeoCode
   include HTTParty
   base_uri "maps.googleapis.com/maps/api/geocode/"
-  default_timeout 15 # 15 second hard timeout
 
 # TODO: This works in development but needs to be done better
-  def api_key
+  def self.api_key
     Rails.application.secrets.google_geocode_key
   end
 
-
-  def timeout_handler
-    begin
-      yield
-    rescue Net::OpenTimeout, Net::ReadTimeout
-      {}
-    end
-  end
-
-  def postal_code(postal)
-    timeout_handler do
-      url = "json?address=#{postal}&key=#{api_key}"
-      self.class.get(url, options)["results"]["geometry"]["location"]["lat"]["lng"]
-    end
+  def self.postal_code(postal, options = {})
+    url = "json?address=#{postal}&key=#{api_key}"
+    GeoCode.get(url, options)
   end
 end
