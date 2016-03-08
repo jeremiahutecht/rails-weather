192873a55ee806db00aa69b5696dec1c6d1ce93e
Jeremiah Utecht <jeremiah@peanutbuttr.com>
2016-03-08 16:08:18 -0600

    Switching branches to update readme

--- lib/geo_code.rb
+++ lib/geo_code.rb
@@ -4,6 +4,11 @@ class GeoCode
   include HTTParty
   base_uri "https://maps.googleapis.com/maps/api/geocode/"
 
+  def initialize
+    location_hash = postal_code
+    assign_values(location_hash)
+  end
+
 # TODO: This works in development but needs to be done better
   def self.api_key
     Rails.application.secrets.google_geocode_key
@@ -12,5 +17,12 @@ class GeoCode
   def self.postal_code(postal)
     url = "/json?address=#{postal}&key=#{api_key}"
     GeoCode.get(url)["results"]
+  end
+
+  def self.assign_values(location_hash)
+    postcode_response = location_hash.parsed_response["results"],["formatted_address"],["geometry"].first
+    self.lat = postcode_response["geometry"]["lat"]
+    self.lng = postcode_response["geometry"]["lng"]
+    self.location = postcode_response["formatted_address"]
   end
 end
